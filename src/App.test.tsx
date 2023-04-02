a5d714e51ebc5d1806899d47a59e198694cebe8b
Bas-Techasit <techasit.bas@gmail.com>
2023-04-02 11:39:19 +0700

    someting happend

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import AddItem from './components/AddItem';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Todo List App.', () => {
+
+  describe('When the user visited websit first time.', () => {
+
+    test('Should be visible the title "Todo List"', () => {
+        render(<App />)
+        const title = screen.getByTestId('title')
+        expect(title.innerHTML).toContain("Todo List")
+    })
+
+    test('The input field should be empty " "', () => {
+      render(<AddItem />)
+      const inputElement: HTMLInputElement = screen.getByTestId('addItemField')
+      expect(inputElement.value).toBe('')
+    })
+  })
+})
