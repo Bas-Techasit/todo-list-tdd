180d0b8cd808f794c8880411c7e61a0c31489cae
Bas-Techasit <techasit.bas@gmail.com>
2023-04-02 19:22:12 +0700

    clean the text field when user clicked add-button

--- src/components/AddItem.tsx
+++ src/components/AddItem.tsx
@@ -1,10 +1,29 @@
+import { useState } from "react";
 
 function AddItem() {
+    const [text, setText] = useState('')
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setText(e.target.value)
+    }    
+
+    function handleSubmit(e: React.FormEvent) {
+        e.preventDefault()
+        setText('')
+    }
+
     return (
-    <>
-        <input data-testid="addItemField"></input>
-        <button data-testid="addButton" disabled>Add</button>
-    </>
+    <form onSubmit={handleSubmit}>
+        <input 
+            data-testid="addItemField"
+            onChange={handleChange}
+            value={text}
+        />
+        <button 
+            data-testid="addButton" 
+            disabled={text === ''} >
+            Add
+        </button>
+    </form>
     )
 }
 
