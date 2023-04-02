0a3c13e6bef672ce69f8c16fe79c6c704fc47e41
Bas-Techasit <techasit.bas@gmail.com>
2023-04-02 12:13:24 +0700

    import AddItem to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import './App.css';
+import AddItem from './components/AddItem';
 
 function App() {
-  return <h1 data-testid="title">Todo List</h1>
+  return (
+    <>
+      <h1 data-testid="title">Todo List</h1>
+      <AddItem />
+    </>
+  )
 }
 
 export default App;
--- src/components/AddItem.tsx
+++ src/components/AddItem.tsx
@@ -1,6 +1,11 @@
 
 function AddItem() {
-    return <input data-testid="addItemField"></input>
+    return (
+    <>
+        <input data-testid="addItemField"></input>
+        <button data-testid="addButton" disabled>Add</button>
+    </>
+    )
 }
 
 export default AddItem
